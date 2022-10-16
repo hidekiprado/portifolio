6c1f7463647a3f0fcf87b6600aaea75f09a222ee
Vinicius Prado <hidekiprado@hotmail.com>
2022-10-16 18:58:59 +1100

    Routing working Creating pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,38 @@
 import "./App.css";
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+
+function Home() {
+  return <div>Home sweet home</div>;
+}
+function About() {
+  return <div>About</div>;
+}
+function Skills() {
+  return <div>Skills</div>;
+}
+function Education() {
+  return <div>Education</div>;
+}
+function Experience() {
+  return <div>Experience</div>;
+}
+function Projects() {
+  return <div>Projects</div>;
+}
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </div>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,35 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 
+const styles = {
+  logo: {
+    width: 50,
+    height: 40,
+  },
+};
+
 const Header = () => {
   return (
     <Navbar variant="dark" bg="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <img
+            to="/"
+            style={styles.logo}
+            src="logo.png"
+            className="d-inline-block align-top"
+            alt="main logo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#education">Education</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/skills">Skills</Nav.Link>
+            <Nav.Link href="/education">Education</Nav.Link>
             <Nav.Link href="#experience">Experience</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
             <NavDropdown title="Contact me" id="basic-nav-dropdown">
               <NavDropdown.Item href="#contact/resume">Resume</NavDropdown.Item>
               <NavDropdown.Divider />
