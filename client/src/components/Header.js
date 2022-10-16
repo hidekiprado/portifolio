30a7008d3be1f2c02f181e5199072100f8c003a6
Vinicius Prado <hidekiprado@hotmail.com>
2022-10-16 17:59:10 +1100

    Header 99% structuring router.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,43 +1,29 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
-import styled, { ThemeContext } from "styled-components";
-import React, { useEffect, useState, useContext } from "react";
-import { NavLink } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 
-const ExternalNavLink = styled.a`
-  color: 'white'
-  &:hover {
-    color: 'black'
-  }
-  &::after {
-    background-color: 'black'
-  }
-`;
-const InternalNavLink = styled(NavLink)`
-  color: 'white';
-  &:hover {
-    color: 'black';
-  }
-  &::after {
-    background-color: 'black;
-  }
-  &.navbar__link--active {
-    color: 'black';
-  }
-`;
 const Header = () => {
-  const theme = useContext(ThemeContext);
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar variant="dark" bg="dark" expand="lg">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Nav
-          activeKey="/home"
-          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-          className="nav-bar-nav"
-        >
-          testing
-        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#education">Education</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <NavDropdown title="Contact me" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#contact/resume">Resume</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#contact/email">
+                E-mail Vinnie
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
